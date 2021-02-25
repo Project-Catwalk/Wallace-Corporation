3c5d8311cd9b35baf57d958326d503416ffe04fa
Adam Whitman <emagdaeh@yahoo.com>
2021-02-25 15:34:53 -0800

    Test suite working with jest

--- __tests__/App.test.jsx
+++ __tests__/App.test.jsx
@@ -1,23 +1,18 @@
-// import React from 'react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from '../client/src/components/App';
+// import { getQueriesForElement } from '@testing-library/dom';
+import { render, screen } from '@testing-library/react';
+
 // import '@testing-library/jest-dom';
 // import { rest } from 'msw';
 // import { setupServer } from 'msw/node';
 // import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 // import '@testing-library/jest-dom/extend-expect';
-// import App from '../client/src/components/App.jsx';
 
-// test('component shoulder render', () => {
-//   expect(sum(1, 2)).toBe(3);
-// });
+test('renders the correct content', () => {
+  const root = document.createElement('div');
+  ReactDOM.render(<App />, root);
 
-import React from 'react';
-import renderer from 'react-test-renderer';
-import { render, screen } from '@testing-library/react';
-import App from '../client/src/components/App.jsx';
-
-describe('App', () => {
-    test('renders App component', () => {
-        render(<App />);
-        screen.getByText('Hello!');
-    });
+  expect(root.querySelector('h1').textContent).toBe('Hello!');
 });
