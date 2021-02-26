2e35064b16f6ef2a2b4d02fac67b57d7eaf44495
Luci Pak <pak.luci.13@gmail.com>
2021-02-26 12:07:24 -0800

    changed imports for Reviews component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import Reviews from '/Reviews.jsx';
+import Reviews from './Reviews.jsx';
 
 
 // Functional React Component:
--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,3 +1,6 @@
+import React from "react";
+import axios from "axios";
+
 const Reviews = () => {
   return <div>hi</div>;
 };
