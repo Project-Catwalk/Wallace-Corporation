abe2bde449f2efed1580e7789ee8fe572a751f13
Luci Pak <pak.luci.13@gmail.com>
2021-02-26 11:08:40 -0800

    added main Reviews.jsx component

--- server/server.js
+++ server/server.js
@@ -1,26 +1,27 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 const port = 3000;
-const axios = require('axios');
-const path = require('path');
-const morgan = require('morgan');
-const ATELIER_API_KEY = require('./config/config.js');
+const axios = require("axios");
+const path = require("path");
+const morgan = require("morgan");
+const ATELIER_API_KEY = require("./config/config.js");
 
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 let options = {
-  url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea',
+  url: "https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea",
   headers: {
     Authorization: `${ATELIER_API_KEY}`,
   },
 };
 
-app.get('/products', (req, res) => {
-
-  axios.get(`${options.url}/products`, options)
+app.get("/products", (req, res) => {
+  
+  axios
+    .get(`${options.url}/products`, options)
     .then((results) => {
       console.log(results.data);
     })
