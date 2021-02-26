94041750d3d02731f7556fbc0706f4d49183d4da
Adam Whitman <emagdaeh@yahoo.com>
2021-02-25 16:27:38 -0800

    Working on checking api with key and website, not working currently

--- server/server.js
+++ server/server.js
@@ -5,10 +5,28 @@ const port = 3000;
 const axios = require('axios');
 const path = require('path');
 const morgan = require('morgan');
+const ATELIER_API_KEY = require('./config/config.js');
 
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(morgan('dev'));
+
+app.get('/products/:product_id', (req, res) => {
+  let options = {
+    url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products`,
+    method: "GET",
+    headers: {
+      "User-Agent": "request",
+      Authorization: `${ATELIER_API_KEY}`,
+    },
+  };
+
+  axios(options)
+    .then((results) => {
+      res.send(results);
+    })
+    .catch(console.error);
+});
 
 app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
