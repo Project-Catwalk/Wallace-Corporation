99cad53831752760cbd7e28e7d6bc38193e038a8
Luci Pak <pak.luci.13@gmail.com>
2021-02-26 11:39:56 -0800

    Added Reviews component to App.jsx to be rendered

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Reviews from '/Reviews.jsx';
 
 
 // Functional React Component:
@@ -8,6 +9,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello!</h1>
+      <Reviews /> 
     </div>
   );
 }
