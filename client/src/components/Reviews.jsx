52a1bc60ab9075ebdab15554181240bd2554f646
Luci Pak <pak.luci.13@gmail.com>
2021-02-27 11:32:09 -0800

    made really simple grid for entire review component

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -1,19 +1,26 @@
-import React from "react";
-import axios from "axios";
-import RatingBreakdown from "./RatingBreakdown.jsx";
-import ReviewsList from "./ReviewsList.jsx";
-import ReviewsSortHeader from "./ReviewsSortHeader.jsx";
-import ReviewsFooter from "./ReviewsFooter.jsx";
+import React from 'react';
+import axios from 'axios';
+import RatingBreakdown from './RatingBreakdown';
+import ReviewsList from './ReviewsList';
+import ReviewsSortHeader from './ReviewsSortHeader';
+import ReviewsFooter from './ReviewsFooter';
+import styles from '../styleComponents/Reviews.module.css';
 
-const Reviews = ({ productId }) => {
-  return (
-    <div>
+const Reviews = ({ productId }) => (
+  <div className={styles.container}>
+    <div className={styles.breakdown}>
       <RatingBreakdown />
+    </div>
+    <div className={styles.header}>
       <ReviewsSortHeader />
+    </div>
+    <div className={styles.individualReview}>
       <ReviewsList />
+    </div>
+    <div className={styles.footer}>
       <ReviewsFooter />
     </div>
-  );
-};
+  </div>
+);
 
 export default Reviews;
--- client/src/components/ReviewsList.jsx
+++ client/src/components/ReviewsList.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import axios from "axios";
+import ReviewTemplate from "./ReviewTemplate.jsx";
 
 const ReviewsList = ({ productId }) => {
-  return <div>ReviewsList</div>;
+  return (
+    <div>
+      ReviewsList
+      <ReviewTemplate />
+    </div>
+  );
 };
 
 export default ReviewsList;
