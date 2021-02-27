d0c36544b76f917ded0453f81fe0007109464cd9
Adam Whitman <emagdaeh@yahoo.com>
2021-02-27 15:46:56 -0800

    Better css grid layout, still needs work

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import styles from './Overview.module.css';
 
-const Overview = () => {
-  return (
-    <div className={styles.wrapper}>
-      <img title ='Display Image' className={styles.one}/>
-      <p className={styles.two}>Read All Reviews</p>
-      <p className={styles.three}>Category</p>
-      <p className={styles.four}>Product Name</p>
-      <p className={styles.five}>Price</p>
-      <form className={styles.six}>Styles</form>
-      <p className={styles.seven}>Description</p>
-      <button className={styles.eight}>Social Media</button>
+const Overview = () => (
+  <div className={styles.wrapper}>
+    <img title ='Display Image' className={styles.one}/>
+    <p className={styles.two}>Read All Reviews</p>
+    <p className={styles.three}>Category</p>
+    <p className={styles.four}>Product Name</p>
+    <p className={styles.five}>Price</p>
+    <div className={styles.six}>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>7</button>
     </div>
-  );
-}
+    <p className={styles.seven}>Description</p>
+    <button className={styles.eight}>Social Media</button>
+  </div>
+);
 
 export default Overview;
