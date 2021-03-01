d38def10357c500b906877344fedb4f88161f295
Luci Pak <pak.luci.13@gmail.com>
2021-03-01 11:28:53 -0800

    fixed date syntax

--- client/src/components/ReviewTemplate.jsx
+++ client/src/components/ReviewTemplate.jsx
@@ -24,11 +24,22 @@ const ReviewTemplate = ({ reviews }) => (
   </div>
 );
 
-const UserDateBar = ({ review }) => (
-  <div>
-    <div className={styles.userDateBar}>{review.reviewer_name}</div>
-    <div className={styles.userDateBar}>{review.date}</div>
-  </div>
-);
+const UserDateBar = ({ review }) => {
+  const getDate = (date) => {
+    const dateArr = date.slice(0, date.indexOf('T')).split('-');
+    const year = dateArr.shift();
+    dateArr.push(year);
+    return dateArr.join('-');
+  };
+
+  const date = getDate(review.date)
+
+  return (
+    <div>
+      <div className={styles.userDateBar}>{review.reviewer_name}</div>
+      <div className={styles.userDateBar}>{date}</div>
+    </div>
+  );
+};
 
 export default ReviewTemplate;
