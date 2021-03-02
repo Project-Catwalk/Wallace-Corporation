bebe56c1b7fb32601e3c944493ca203e2d26a809
Jesse <jesseholden713@gmail.com>
2021-03-01 17:29:05 -0800

    Implemented Modal for my add question button with basic css styling Added a hover to all the buttons set to pointer

--- client/src/components/qa/QuestionModal.jsx
+++ client/src/components/qa/QuestionModal.jsx
@@ -1,25 +1,59 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import styles from '../../styleComponents/App.module.css';
+// import styles from '../../styleComponents/QA.modules.css';
 
-const QuestionModal = () => {
-  return (
+// function QuestionModal() {
+//   return (
+//     <>
+//       <div>
+//         <h3>Ask Your Question</h3>
+//         <h2>About the "product"</h2>
+//       </div>
+//       <div>
+//         <form action="">
+//           <p>Question: </p>
+//           <input type="text" />
+//           <p>What is your nickname?</p>
+//           <input placerholder="example: jackson11!" type="text" />
+//           <p>email</p>
+//           <input type="text" />
+//           <button>Submit</button>
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
+
+function Modal({ open, children, onClose }) {
+  if (!open) {
+    return null;
+  }
+
+  return ReactDOM.createPortal(
     <>
-    <div>
-        <h3>Ask Your Question</h3>
-        <h2>About the "product"</h2>
-    </div>
-    <div>
-      <form action="">
-        <p>Question: </p>
-        <input type="text"/>
-        <p>What is your nickname?</p>
-        <input placerholder="example: jackson11!" type="text"/>
-        <p>email</p>
-        <input type="text"/>
-        <button>Submit</button>
-      </form>
-    </div>
-    </>
-  )
-};
+      <div className={styles.overlay}></div>
+      <div className={styles.modal}>
+        <div className={styles.modalHeader}>
+          <h3>Ask Your Question about the Product</h3>
+          <p className={styles.closeModal} onClick={onClose}>x</p>
+        </div>
+        <div>
+          <form action="">
+            <p>Question:</p>
+            <textarea maxLength="1000" className={styles.qInput} type="text" />
+            <p>What is your nickname?</p>
+            <input placeholder="example: jackson11!" type="text" />
+            <p>email</p>
+            <input type="text" />
+            <div />
+            <button className={styles.modalButton} >Submit</button>
+          </form>
+        </div>
+      </div>
+    </>,
+    document.getElementById('modal'),
+  );
+}
 
-export default QuestionModal;
+export default Modal;
