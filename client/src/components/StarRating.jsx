98cb5209cf828643fca24c2de1a85c98a0d1fd96
Luci Pak <pak.luci.13@gmail.com>
2021-03-02 08:35:05 -0800

    continuing functionality of star component

--- client/src/components/StarRating.jsx
+++ client/src/components/StarRating.jsx
@@ -1,36 +1,23 @@
 import React from 'react';
 import styles from '../styleComponents/Reviews.module.css';
 
-const StarRating = ({ reviews }) => {
-  const averageRating = () => {
-    const reducedRating = reviews.reduce((acc, review) => {
-      const total = acc + (review.rating / 5) * 100;
-      return total;
-    }, 0);
-    const average = reducedRating / reviews.length;
-    const roundedAvg = (Math.round(average));
-    const avg = Math.round(roundedAvg / 5) * 5;
-    return avg;
-  };
-
-  return (
-    <div className={styles.starRating}>
-      <div className={styles.starRatingTop} style={{ width: averageRating() }}>
-        <span>★</span>
-        <span>★</span>
-        <span>★</span>
-        <span>★</span>
-        <span>★</span>
-      </div>
-      <div className={styles.starRatingBottom}>
-        <span>★</span>
-        <span>★</span>
-        <span>★</span>
-        <span>★</span>
-        <span>★</span>
-      </div>
+const StarRating = ({ average }) => (
+  <div className={styles.starRating}>
+    <div className={styles.starRatingTop} style={{ width: average() }}>
+      <span>★</span>
+      <span>★</span>
+      <span>★</span>
+      <span>★</span>
+      <span>★</span>
     </div>
-  );
-};
+    <div className={styles.starRatingBottom}>
+      <span>★</span>
+      <span>★</span>
+      <span>★</span>
+      <span>★</span>
+      <span>★</span>
+    </div>
+  </div>
+);
 
 export default StarRating;
--- client/src/components/reviews/RatingBreakdown.jsx
+++ client/src/components/reviews/RatingBreakdown.jsx
@@ -3,8 +3,23 @@ import StarRating from '../StarRating';
 // import axios from "axios";
 // import styles from '../styleComponents/Reviews.module.css';
 
-const RatingBreakdown = ({ reviews }) => (
-  <StarRating reviews={reviews} />
-);
+const RatingBreakdown = ({ reviews }) => {
+  const averageRating = () => {
+    const reducedRating = reviews.reduce((acc, review) => {
+      const total = acc + (review.rating / 5) * 100;
+      return total;
+    }, 0);
+    const average = reducedRating / reviews.length;
+    const roundedAvg = (Math.round(average));
+    const avg = Math.round(roundedAvg / 5) * 5;
+    return avg;
+  };
 
+  return (
+    <div>
+      <h3>Ratings & Reviews</h3>
+      <StarRating average={averageRating} />
+    </div>
+  );
+};
 export default RatingBreakdown;
