5346d2ca4b2cfcf560d8d4b0e8f1c9162c11deed
Jesse <jesseholden713@gmail.com>
2021-03-02 17:45:17 -0800

    Worked on my Question Modal and Answer Modal Almost completed, added fine print messages and required popups for invalid email and empty fields Successfully implemented functionality for clearing forms upon either submitting, xing out, or clicking away from form

--- client/src/components/qa/AnswerModal.jsx
+++ client/src/components/qa/AnswerModal.jsx
@@ -9,6 +9,17 @@ function Modal({open, onClose, question_id, getQuestions, productId}) {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [photos, setPhotos] = useState([]);
+  const [error, setError] = useState('');
+
+  const validEmailRegex = RegExp(
+    /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+  );
+
+  const clearForm = (e) => {
+    setAnswer('');
+    setUsername('');
+    setEmail('');
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,14 +29,27 @@ function Modal({open, onClose, question_id, getQuestions, productId}) {
       email: email,
       photos: photos,
     };
+    if (!validEmailRegex.test(email)) {
+      setError('*You must enter a valid email');
+      return;
+    }
     axios.post(`/qa/questions/${question_id}/answers`, answerInfo)
       .then(() => getQuestions(productId))
+      .then(() => clearForm())
       .catch(console.log);
   };
 
   return ReactDOM.createPortal(
     <>
-      <div onClick={onClose} className={open ? styles.overlay : ''} ></div>
+      <div 
+        onClick={() => {
+          onClose();
+          clearForm();
+        }} 
+        className={open ? styles.overlay : ''} 
+      >
+
+      </div>
       <div style={{
           transform: open ? 'translate(-50%, -50%)' : 'translate(-50%, -150vh)'
       }}
@@ -33,18 +57,31 @@ function Modal({open, onClose, question_id, getQuestions, productId}) {
       >
         <div className={styles.modalHeader}>
           <h3>Add an Answer</h3>
-          <p className={styles.closeModal} onClick={onClose}>x</p>
+          <p 
+            className={styles.closeModal}
+            onClick={() => {
+              onClose();
+              clearForm();
+            }}
+          >
+            x
+          </p>
         </div>
         <div className={styles.modalBody}>
           <form onSubmit={(e) => handleSubmit(e)} action="">
-            <p>Your Answer</p>
-            <textarea onChange={(e) => setAnswer(e.target.value)} className={styles.qInput} maxLength="1000" />
-            <p>What is your nickname?</p>
-            <input onChange={(e) => setUsername(e.target.value)} className={qastyles.modalInput} type="text" placeholder="Example: jack543!" />
-            <p>Your Email</p>
-            <input onChange={(e) => setEmail(e.target.value)} className={qastyles.modalInput} type="text" placeholder="Example: jack@email.com" />
-            <button>Add Photos</button>
+            <p>Your Answer *</p>
+            <textarea value={answer} required="required" onChange={(e) => setAnswer(e.target.value)} className={styles.qInput} maxLength="1000" />
+            <p>What is your nickname? *</p>
+            <input value={username} required="required" onChange={(e) => setUsername(e.target.value)} className={qastyles.modalInput} type="text" placeholder="Example: jack543!" />
+            <p className={qastyles.finePrint}>{username.length > 0 ? 'For privacy reasons, do not use your full name or email address' : ''}</p>
+            <p>Your Email *</p>
+            <input value={email} required="required" onChange={(e) => setEmail(e.target.value)} className={qastyles.modalInput} type="text" placeholder="Example: jack@email.com" />
+            <p className={qastyles.finePrint}>{email.length > 0 ? 'For authentication reasons, you will not be emailed' : ''}</p>
+            <div>
+              <button>Add Photos</button>
+            </div>
             <button type="submit" className={styles.modalButton}>Submit Answer</button>
+            <p className={qastyles.finePrint}>{error}</p>
           </form>
         </div>
       </div>
