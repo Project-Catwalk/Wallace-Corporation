87254c317d3978b79d560b1d3d0614823fbd58ff
Adam Whitman <emagdaeh@yahoo.com>
2021-03-06 13:33:28 -0800

    Zoom in and out working on main display expanded image, need to get scrolling working

--- client/src/components/overview/OverviewExpandedModal.jsx
+++ client/src/components/overview/OverviewExpandedModal.jsx
@@ -1,19 +1,44 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import styles from '../../styleComponents/Overview.module.css';
 
 const OverviewExpandedModal = (props) => {
-
   const { children, open, close } = props;
+
+  const [height, setHeight] = useState(null);
+  const [width, setWidth] = useState(null);
+  const [isZoomed, setIsZoomed] = useState(false);
+  const [cursorString, setcursorString] = useState('zoom-in');
+
+  const imgRef = useRef();
 
   if (!open) {
     return null;
   }
 
+  const zoom = (event) => {
+    event.preventDefault();
+
+    const currentHeight = imgRef.current.clientHeight;
+    const currentWidth = imgRef.current.clientWidth;
+
+    if (isZoomed === false) {
+      setIsZoomed(true);
+      setHeight(currentHeight * 2.5);
+      setWidth(currentWidth * 2.5);
+      setcursorString('zoom-out');
+    } else {
+      setIsZoomed(false);
+      setHeight(currentHeight / 2.5);
+      setWidth(currentWidth / 2.5);
+      setcursorString('zoom-in');
+    }
+  };
+
   return (
     <>
       <div className={styles.modalOverlay} />
-      <div className={styles.modal}>
+      <div className={styles.modal} ref={imgRef} onClick={zoom} style={{ height, width, cursor: cursorString}}>
         {children}
         <button type="submit" onClick={close}>Close</button>
       </div>
