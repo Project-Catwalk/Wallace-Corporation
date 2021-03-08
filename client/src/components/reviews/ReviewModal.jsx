8e3be93b589e8ecdd5ec4e3dae0e654ee55bba20
Luci Pak <pak.luci.13@gmail.com>
2021-03-08 15:41:48 -0800

    fixed overlay of reviews modal

--- client/src/components/reviews/ReviewModal.jsx
+++ client/src/components/reviews/ReviewModal.jsx
@@ -105,7 +105,7 @@ const ReviewsModal = ({
       ? (
         <>
           <div
-            style={{ maxHeight: '80%' }}
+            style={{ maxHeight: '100%' }}
             role="presentation"
             onClick={() => {
               onClose();
